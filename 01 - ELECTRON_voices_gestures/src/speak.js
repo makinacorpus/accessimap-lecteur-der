74a1f16ecbfc173da1d8265e009d159cd25c0d37
Alexandra Janin <alexandra.janin@makina-corpus.com>
2016-05-02 19:55:30 +0200

    Fix Proto2

--- 01 - ELECTRON_voices_gestures/src/speak.js
+++ 01 - ELECTRON_voices_gestures/src/speak.js
@@ -42,12 +42,13 @@ function showEvent(event) {
 function nativeSpeak(text) {
     var msg = new SpeechSynthesisUtterance();
     var voices = speechSynthesis.getVoices();
-    msg.voice = voices[0]; // Note: some voices don't support altering params
-    msg.voiceURI = 'native';
+    // msg.voice = voices[0]; // Note: some voices don't support altering params
+    // msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
     msg.rate = 1; // 0.1 to 10
     msg.pitch = 1; //0 to 2
     msg.text = text;
     msg.lang = 'fr';
     speechSynthesis.speak(msg);
+    console.log(msg);
 }
