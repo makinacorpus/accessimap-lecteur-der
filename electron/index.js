bec3dcf5519703d1f65916aeca80e0528b2be6bf
bastien <bastien.alvez@gmail.com>
2017-02-13 16:47:28 +0100

    default mode

--- electron/index.js
+++ electron/index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // Dev
-// var webspeechapi = require('./../modules/tts.webapi/tts.webapi.js');
-// var vibrateWebApi = require('./../modules/vibrate.webapi/vibrate.webapi.js');
-// var DerReader = require('./../modules/der-reader/der-reader.js');
-//
+ var webspeechapi = require('./../modules/tts.webapi/tts.webapi.js');
+ var vibrateWebApi = require('./../modules/vibrate.webapi/vibrate.webapi.js');
+ var DerReader = require('./../modules/der-reader/der-reader.js');
+
 // Bundle
-var webspeechapi = require('tts.webapi');
-var vibrateWebApi = require('vibrate.webapi');
-var DerReader = require('der-reader');
+// var webspeechapi = require('tts.webapi');
+// var vibrateWebApi = require('vibrate.webapi');
+// var DerReader = require('der-reader');
 
 var remote = require('electron').remote;
 
@@ -27,6 +27,6 @@ DerReader.init({
   derFile: './der/Londres.zip',
   tts: webspeechapi,
   vibrate: vibrateWebApi,
-  defaultMode: 'explore',
+  defaultMode: 1,
   exit: exit
 });
