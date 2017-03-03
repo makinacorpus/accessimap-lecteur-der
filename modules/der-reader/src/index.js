30b30e1e7c9ccb1b35c96bfe6f77b160922e4b3a
Mathieu DARTIGUES <mathieu@dartic.fr>
2017-03-03 17:49:47 +0100

    Adding a web demo in der-reader module easier for debug purposes

---
+++ modules/der-reader/src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var webspeechapi, vibrateWebApi, DerReader;
+
+webspeechapi = require('./../../tts.webapi/tts.webapi.js');
+vibrateWebApi = require('./../../vibrate.webapi/vibrate.webapi.js');
+DerReader = require('./der-reader.js');
+
+DerReader.init({
+    container: 'der-reader',
+    derFile: null,
+    tts: webspeechapi,
+    vibrate: vibrateWebApi,
+    defaultMode: 0,
+    exit: function() {}
+});
