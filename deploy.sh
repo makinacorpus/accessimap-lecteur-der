b9a0bd48c1bc4feb806adb3371e6416495e95d0e
Alexandra Janin <alexandra.janin@makina-corpus.com>
2017-04-13 17:43:42 +0200

    Add log on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
-set -o errexit
 
 # config
 git config --global user.email "travis@nobody.org"
@@ -8,7 +7,9 @@ git config --global user.name "Travis CI"
 
 # deploy
 cd modules/der-reader/dist
+ls
 git init
 git add .
 git commit -m "Deploy to Github Pages"
+git status
 git push --force --quiet "https://${GITHUB_API_TOKEN}@$github.com/${GITHUB_REPO}.git" refactor-webpack:gh-pages > /dev/null 2>&1
