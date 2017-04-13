0bb2a878be8e130cf012aff3dce213f0e426a531
Alexandra Janin <alexandra.janin@makina-corpus.com>
2017-04-13 11:32:23 +0200

    Add deploy script for github-pages

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+# config
+git config --global user.email "travis@nobody.org"
+git config --global user.name "Travis CI"
+
+# deploy
+cd dist
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" refactor-webpack:gh-pages > /dev/null 2>&1
